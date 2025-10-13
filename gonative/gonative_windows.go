24cd059295814a9842f2af5e08680c1bebef851e
Oleksii Ursulenko <alexeyu@microsoft.com>
2025-10-13 10:48:10 -0700

    add callback

--- gonative/gonative_windows.go
+++ gonative/gonative_windows.go
@@ -4,19 +4,24 @@ package gonative
 
 import (
 	_ "embed"
+	"fmt"
 	"os"
 	"path/filepath"
+	"syscall"
+	"unsafe"
 
 	"golang.org/x/sys/windows"
 )
 
 type nativeAPI struct {
-	factorial *windows.LazyProc
+	factorial        *windows.LazyProc
+	set_log_callback *windows.LazyProc
 }
 
 //go:embed TestNativeLibrary.dll
 var dllBytes []byte
 var api *nativeAPI
+var loggingCallback uintptr
 
 func initNativeAPI() {
 
@@ -38,8 +43,15 @@ func initNativeAPI() {
 		panic("Failed to find 'factorial' procedure in DLL: " + err.Error())
 	}
 
+	setLogCallbackProc := dll.NewProc("set_log_callback")
+	err = setLogCallbackProc.Find()
+	if err != nil {
+		panic("Failed to find 'set_log_callback' procedure in DLL: " + err.Error())
+	}
+
 	api = &nativeAPI{
-		factorial: factorialProc,
+		factorial:        factorialProc,
+		set_log_callback: setLogCallbackProc,
 	}
 }
 
@@ -66,10 +78,35 @@ func loadLibrary() (*windows.LazyDLL, error) {
 	return dll, nil
 }
 
+func utf8CStringToString(p *byte) string {
+	if p == nil {
+		return ""
+	}
+	// Find the length up to the first 0 byte.
+	var n int
+	for *(*byte)(unsafe.Add(unsafe.Pointer(p), n)) != 0 {
+		n++
+	}
+	// Convert to a slice and then to string.
+	bytes := unsafe.Slice(p, n)
+	return string(bytes)
+}
+
+func enableLogging() {
+	loggingCallback = syscall.NewCallback(func(logLevel int, message *byte, pii int) uintptr {
+		logMessage := utf8CStringToString(message)
+		fmt.Printf("[Level %d] %s\n", logLevel, logMessage)
+		return 0
+	})
+
+	api.set_log_callback.Call(loggingCallback)
+}
+
 // Exports
 
 func Factorial(n int) int {
 	initNativeAPI()
+	enableLogging()
 
 	// Call the factorial function from the DLL
 	result, _, lastErr := api.factorial.Call(uintptr(n))
